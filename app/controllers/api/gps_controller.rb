60e8f63acaab92651cb67197b7dc3a672c65294e
bdt17 <brett.thomas29.97@gmail.com>
2026-01-17 14:39:44 -0700

    Phase 14 GPS controller only

--- app/controllers/api/gps_controller.rb
+++ app/controllers/api/gps_controller.rb
@@ -1,21 +1,20 @@
+
 module Api
   class GpsController < ApplicationController
     skip_before_action :verify_authenticity_token
     
     def create
-      vehicle = Vehicle.find_or_create_by(batch_id: params[:batch_id].to_i) do |v|
-        v.lat = params[:lat]&.to_f || 0.0
-        v.lng = params[:lng]&.to_f || 0.0
-        v.speed = params[:speed]&.to_f || 0.0
-        v.heading = params[:heading]&.to_f || 0.0
-      end
-      
-      vehicle.touch
+      # Simple response - no DB needed yet
       render json: {
         status: 'received',
-        vehicle_id: vehicle.id,
-        position: { lat: vehicle.lat, lng: vehicle.lng, speed: vehicle.speed }
+        message: 'Phase 14 GPS LIVE',
+        position: {
+          lat: params[:lat]&.to_f || 0.0,
+          lng: params[:lng]&.to_f || 0.0,
+          speed: params[:speed]&.to_f || 0.0
+        }
       }, status: :ok
     end
   end
 end
+
