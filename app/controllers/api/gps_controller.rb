b3070d8aabe1802821b19978422c4d8d67238d2f
bdt17 <brett.thomas29.97@gmail.com>
2026-01-17 01:33:34 -0700

    Phase 14 GPS controller + Vehicle model (integer batch_id)

--- app/controllers/api/gps_controller.rb
+++ app/controllers/api/gps_controller.rb
@@ -1,19 +1,21 @@
 module Api
   class GpsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
     def create
-      vehicle = Vehicle.find_or_initialize_by(batch_id: params[:batch_id])
-      vehicle.update!(
-        lat: params[:lat].to_f,
-        lng: params[:lng].to_f, 
-        speed: params[:speed].to_f,
-        heading: params[:heading]&.to_f || 0
-      )
+      vehicle = Vehicle.find_or_create_by(batch_id: params[:batch_id].to_i) do |v|
+        v.lat = params[:lat]&.to_f || 0.0
+        v.lng = params[:lng]&.to_f || 0.0
+        v.speed = params[:speed]&.to_f || 0.0
+        v.heading = params[:heading]&.to_f || 0.0
+      end
       
-      render json: { 
-        status: 'received', 
+      vehicle.touch
+      render json: {
+        status: 'received',
         vehicle_id: vehicle.id,
-        position: { lat: vehicle.lat, lng: vehicle.lng }
-      }, status: :created
+        position: { lat: vehicle.lat, lng: vehicle.lng, speed: vehicle.speed }
+      }, status: :ok
     end
   end
 end
