9a6a0e42d1917cdf9fd6edab70cdbdaf6c6ee8aa
bdt17 <brett.thomas29.97@gmail.com>
2026-01-17 16:20:37 -0700

    Fix production.rb syntax + inline pharma dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   root "dashboard#index"
-  get "dashboard", to: "dashboard#index", as: :dashboard_index
-  
-  resources :vehicles, only: [:index, :show]
-  resources :batches, only: [:index, :show] 
-  resources :alerts, only: [:index, :show]
-  get "revenue", to: "revenue#index", as: :revenue
 end
