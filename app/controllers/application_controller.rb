209aa2ee715f92bdb124d539b0a638431b64d21d
bdt17 <brett.thomas29.97@gmail.com>
2026-01-17 16:14:11 -0700

    Fix routes.rb syntax + dashboard controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,2 @@
 class ApplicationController < ActionController::API
 end
-layout 'dashboard'
--- app/helpers/application_helper.rb
+++
@@ -1,3 +0,0 @@
-def nav_link(text, path)
-  link_to text, path, class: "px-4 py-2 rounded-xl font-semibold transition-all duration-200 bg-white/10 hover:bg-white/20 backdrop-blur-sm border border-white/20 hover:border-white/40"
-end
