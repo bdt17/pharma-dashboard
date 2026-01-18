2b9ddb495eb1b5aea6b8782594ef9e1e6ddaeab0
bdt17 <brett.thomas29.97@gmail.com>
2026-01-18 15:55:52 -0700

    TEMP: Remove API controllers

--- app/controllers/api/base_controller.rb
+++
@@ -1,3 +0,0 @@
-class Api::BaseController < ActionController::API
-  # Shared API logic here
-end
--- app/controllers/api/marketplace_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::MarketplaceController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  # protect_from_forgery removed - API controllers don't need CSRF
-  
-  def index
-    render json: { status: 'Phase 14 Marketplace API LIVE' }
-  end
-end
