04edfc2d78ad7ee8f241729d192b3ffc4298f626
bdt17 <brett.thomas29.97@gmail.com>
2026-01-17 17:23:00 -0700

    ✅ Phase 8 PDF: 21 CFR Part 11 Chain of Custody LIVE

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,1 +1,22 @@
 class BatchesController < ApplicationController; def index; @title = 'Batches'; end; end
+
+  def chain_of_custody
+    @batch = Batch.find(params[:id])
+    respond_to do |format|
+      format.pdf do
+        html = render_to_string(template: "batches/chain_of_custody", layout: "pdf")
+        pdf = WickedPdf.new.pdf_from_string(
+          html,
+          footer: {
+            center: "LOT-#{@batch.id} [Page %{page} of %{numpages}]",
+            font_size: 10,
+            spacing: 5
+          }
+        )
+        send_data pdf, 
+                  filename: "CoC-PHARMA-#{@batch.id}-#{Time.current.strftime('%Y%m%d')}.pdf", 
+                  type: "application/pdf",
+                  disposition: "attachment"
+      end
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,1 +1,2 @@
 Rails.application.routes.draw { root "dashboard#index" }
+  get 'batches/:id/chain_of_custody', to: 'batches#chain_of_custody', as: :batch_chain_of_custody
