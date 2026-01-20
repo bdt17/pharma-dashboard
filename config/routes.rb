97a6c1623f7078f8fe01d6d8de63a7584dcf49cb
bdt17 <brett.thomas29.97@gmail.com>
2026-01-19 19:20:19 -0700

    🚀 PHASE 18 B2B2C PORTALS - Pharmacist + Patient login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "landing#index"
   get "/gps", to: "gps#dashboard"
+  get '/pharmacists/sign_in', to: 'pharmacists#new'
+  get '/patients/sign_in', to: 'patients#new'
 end
