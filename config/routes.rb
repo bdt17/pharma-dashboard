9e70209d6fd56e52ba1c832ae5e50ae462589c46
bdt17 <brett.thomas29.97@gmail.com>
2026-01-20 00:59:01 -0700

    ✅ PUBLIC DASHBOARD LIVE

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   root "batches#index"
-  resources :batches
-  # devise_for :users
+  resources :batches, except: [:index]
 end
-get "/", to: "batches#index", constraints: { format: :html }
