2c895cb71ced2dbedc41ed5efaa54f6cb7f96547
bdt17 <brett.thomas29.97@gmail.com>
2026-01-19 19:59:57 -0700

    🚀 PHASE 18 B2B2C LIVE - Clean boot

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   root "landing#index"
   get '/pharmacists', to: 'landing#pharmacists'
   get '/patients', to: 'landing#patients'
-  get '/gps', to: 'gps#dashboard'
 end
