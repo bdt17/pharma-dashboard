7e492278efdbfd756a34405f7814dfb32f2189f1
bdt17 <brett.thomas29.97@gmail.com>
2026-01-19 19:43:33 -0700

    ✅ PHASE 18 CLEAN - No Devise crash

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :patients
-  devise_for :pharmacists
   root "landing#index"
-  get "/gps", to: "gps#dashboard"
-  get '/pharmacists/sign_in', to: 'pharmacists#new'
-  get '/patients/sign_in', to: 'patients#new'
+  get '/pharmacists', to: 'landing#pharmacists'
+  get '/patients', to: 'landing#patients'
+  get '/gps', to: 'gps#dashboard'
 end
