36d3735d89a825ce95c6f23cfaf85e6c6fd86226
bdt17 <brett.thomas29.97@gmail.com>
2026-01-20 00:02:31 -0700

    ✅ FIXED

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "batches#index"
   resources :batches
-  # TEMP DISABLED: devise_for :users :users
+  # TEMP DISABLED: # TEMP DISABLED: devise_for :users :users :users
 end
