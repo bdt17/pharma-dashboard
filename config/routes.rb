7e9fd639a92f5edb50923512b6524999c933d942
bdt17 <brett.thomas29.97@gmail.com>
2026-01-20 22:57:44 -0700

    Phase 23.12: BULLETPROOF pharma dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/revenue-test", to: "homepage#revenue_test"
-  get "/coc/:id", to: "application#chain_of_custody"  # FDA PDFs
+  get "/coc/:id", to: "application#chain_of_custody"
   root "homepage#index"
 end
