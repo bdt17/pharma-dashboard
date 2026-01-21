5f8c88423b13aaa0e6a75a117cdb4436ad99c60e
bdt17 <brett.thomas29.97@gmail.com>
2026-01-20 20:40:15 -0700

    Phase 21.9: Dedicated PdfsController + clean routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # Phase 21: FDA Revenue PDFs via ApplicationController
-  get '/batches/:id/chain_of_custody', to: 'application#chain_of_custody'
+  get '/batches/:id/chain_of_custody', to: 'pdfs#chain_of_custody'
   root "homepage#index"
 end
