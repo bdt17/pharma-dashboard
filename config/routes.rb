d7d22f2282de0fb465f67fdb5e1033b63552bc1b
bdt17 <brett.thomas29.97@gmail.com>
2026-01-20 20:31:20 -0700

    Phase 21.6: FIXED routes.rb SYNTAX - Rails.application.routes.draw

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
-# FDA Revenue Phase 21
-get "/batches/:id/chain_of_custody", to: "revenue#chain_of_custody"
 Rails.application.routes.draw do
-  root "home#index"
-end
-
-get '/batches/:id/chain_of_custody', to: 'revenue#chain_of_custody'
-get '/batches/:id/label', to: 'revenue#shipping_label'  
-get '/batches/:id/manifest', to: 'revenue#manifest'
-
-get "/batches/:id/chain_of_custody", to: "revenue#chain_of_custody", as: :batch_chain_of_custody
-get "/batches/:id/label", to: "revenue#shipping_label", as: :batch_label
-get "/batches/:id/manifest", to: "revenue#manifest", as: :batch_manifest
-
+  # Phase 21: FDA Revenue PDFs
+  get '/batches/:id/chain_of_custody', to: 'revenue#chain_of_custody'
+  get '/batches/:id/label', to: 'revenue#shipping_label'
+  get '/batches/:id/manifest', to: 'revenue#manifest'
+  
+  # Existing routes below
+  root "homepage#index"
