3ffa0a915e8004c340ffac02c777669aa6bc1a4a
bdt17 <brett.thomas29.97@gmail.com>
2026-01-21 20:39:30 -0700

    Minimal stable pharma dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    render plain: "<h1>PHARMA DASHBOARD LIVE - 127 Batches 24 Vehicles</h1><p>PHX-001 Scottsdale ETA 8min</p>", layout: false
+    @batches = 127
+    @vehicles = 24
+    @revenue = '$12K'
   end
 end
