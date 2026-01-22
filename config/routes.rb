79112d47a024864df1b67ccfe0b79b356de20c9d
bdt17 <brett.thomas29.97@gmail.com>
2026-01-22 15:48:58 -0700

    Temporary disable Devise routes - fix User autoload

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get "/batches", to: "batches#index"
-
-
-  devise_for :users
+ #  devise_for :users
   root "dashboard#index"
   get '/dashboard', to: 'dashboard#index'
   get '/batches', to: 'dashboard#batches'
