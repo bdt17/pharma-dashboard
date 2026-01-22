f2f53f04b6e4fcf54ccb229c8625e20950a9224e
bdt17 <brett.thomas29.97@gmail.com>
2026-01-21 17:57:18 -0700

    Fix Puma config - remove invalid host method

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,4 @@
 port        ENV.fetch("PORT") { 3000 }
-host        ENV.fetch("HOST") { "0.0.0.0" }
 environment ENV.fetch("RAILS_ENV") { "production" }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
