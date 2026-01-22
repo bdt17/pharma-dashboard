ce302b0bf968db1f59e275272685629c6e6ca4ad
bdt17 <brett.thomas29.97@gmail.com>
2026-01-21 19:28:22 -0700

    Fix puma PORT for Render

--- config/puma.rb
+++ config/puma.rb
@@ -2,8 +2,5 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "production" }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
-
 preload_app!
-
-# Rails 8 importmap - no workers needed for static dashboard
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
