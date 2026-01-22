0eb1906936fa6500c193fc998b5dcc2b17916589
bdt17 <brett.thomas29.97@gmail.com>
2026-01-21 19:20:13 -0700

    Puma direct start - Rails 8 pharma dashboard

--- config/puma.rb
+++ config/puma.rb
@@ -2,4 +2,8 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "production" }
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
+
 preload_app!
+
+# Rails 8 importmap - no workers needed for static dashboard
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
