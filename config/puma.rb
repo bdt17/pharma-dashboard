0e967e53571d4f6357f7480cfcbb0d67372c744e
bdt17 <brett.thomas29.97@gmail.com>
2026-01-21 20:31:57 -0700

    Puma single mode production

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,4 @@
-port        ENV.fetch("PORT")
-environment "production"
-threads     1, 1
-
-# SINGLE MODE - no cluster warnings
 workers 0
-silence_single_worker_warning
+threads 1, 1
+environment 'production'
+port ENV.fetch("PORT") { 10000 }
