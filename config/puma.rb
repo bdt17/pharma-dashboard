95dc3c761bc43377364fda46c9a7132967b62049
bdt17 <brett.thomas29.97@gmail.com>
2026-01-21 17:46:07 -0700

    Reset routes.rb and puma.rb - pure Ruby syntax

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 port        ENV.fetch("PORT") { 3000 }
 host        ENV.fetch("HOST") { "0.0.0.0" }
 environment ENV.fetch("RAILS_ENV") { "production" }
-threads 2, 6
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
 preload_app!
