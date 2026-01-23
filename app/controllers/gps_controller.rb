74c07832761a41733ed55051e1034f2f35e7a368
bdt17 <brett.thomas29.97@gmail.com>
2026-01-23 14:02:52 -0700

    GPS strong params COMPLETE - 5/5 APIs

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -1,7 +1,13 @@
 class GpsController < ApplicationController
   def update
     gps_params = params.permit(:lat, :lng, :batch)
-    render json: {status: 'received', lat: gps_params[:lat], lng: gps_params[:lng], 
-                  batch: gps_params[:batch] || 'B001', vehicles: 24, location: 'Phoenix AZ'}
+    render json: {
+      status: 'received',
+      lat: gps_params[:lat],
+      lng: gps_params[:lng],
+      batch: gps_params[:batch] || 'B001',
+      vehicles: 24,
+      location: 'Phoenix AZ'
+    }
   end
 end
