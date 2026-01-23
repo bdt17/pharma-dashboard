8ca454dbf57b9f022b30af89417a99660a1f2d0b
bdt17 <brett.thomas29.97@gmail.com>
2026-01-22 18:24:59 -0700

    Phase 8: Re-enable Devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/reports/chain-of-custody/:id', to: 'reports#chain_of_custody', as: :chain_of_custody
   get "/batches", to: "batches#index"
- #  devise_for :users
+  devise_for :users
   root "dashboard#index"
   get '/dashboard', to: 'dashboard#index'
   get '/batches', to: 'dashboard#batches'
