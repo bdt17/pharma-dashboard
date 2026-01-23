6bb887fc05094c65f86f2a49ebf8a924f1c47968
bdt17 <brett.thomas29.97@gmail.com>
2026-01-22 18:29:46 -0700

    🚨 FIX: Comment out Devise (no User model)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/reports/chain-of-custody/:id', to: 'reports#chain_of_custody', as: :chain_of_custody
   get "/batches", to: "batches#index"
-  devise_for :users
+  # devise_for :users  # ← COMMENTED OUT (no User model)
   root "dashboard#index"
   get '/dashboard', to: 'dashboard#index'
   get '/batches', to: 'dashboard#batches'
