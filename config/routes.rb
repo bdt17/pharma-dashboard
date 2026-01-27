ce741bd674845a0f6d8d309774ecb3840bdf6337
bdt17 <brett.thomas29.97@gmail.com>
2026-01-27 15:22:28 -0700

    Phase 8: Proper routes + controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   post '/api/gps', to: 'gps#update'
   get '/api/gps/stream', to: 'gps#stream'
   get '/api/health', to: 'gps#health'
+
+  get '/test-pdf', to: 'reports/chain_of_custody#index'
 end
