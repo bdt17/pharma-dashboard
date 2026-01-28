2b4f1e30255cf0b358a5ca14ad1b1ef32a7cf74f
bdt17 <brett.thomas29.97@gmail.com>
2026-01-28 11:57:03 -0700

    fix routes.rb syntax

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get '/test-ui', to: 'tests#ui'
   get '/test-rails', to: 'tests#rails'
   get '/test-infosec', to: 'tests#infosec'
-end
+
 get '/dashboard/v2', to: 'dashboard#v2'
+end
