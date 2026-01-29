ea476b9a191dd840354c8aa761c3fcbc12eaae1b
bdt17 <brett.thomas29.97@gmail.com>
2026-01-28 20:54:27 -0700

    nuclear GPS POST fix: no model dependency

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -1,32 +1,23 @@
 class GpsController < ApplicationController
-  skip_before_action :verify_authenticity_token # GPS devices = no CSRF
+  skip_before_action :verify_authenticity_token
+  protect_from_forgery prepend: false, except: [:update]
 
   def update
-    imei = params[:imei]
+    imei = params[:imei]&.strip
     lat = params[:lat]&.to_f
     lng = params[:lng]&.to_f
 
-    unless imei && lat && lng
-      return head :unprocessable_entity 
-    end
+    # Log incoming data
+    Rails.logger.info "GPS: imei=#{imei} lat=#{lat} lng=#{lng}"
 
-    # Update/Create vehicle location
-    vehicle = Vehicle.find_or_initialize_by(imei: imei)
-    vehicle.update(lat: lat, lng: lng, last_ping: Time.current)
-    vehicle.save!
-
-    # Real-time broadcast (ActionCable)
-    ActionCable.server.broadcast 'gps_channel', {
-      imei: imei,
-      lat: lat,
+    # Simple JSON response - NO DATABASE
+    render json: { 
+      status: 'received', 
+      imei: imei, 
+      lat: lat, 
       lng: lng,
-      timestamp: Time.current.to_i
-    }
-
-    head :ok
-  rescue => e
-    Rails.logger.error "GPS Update Error: #{e.message}"
-    head :unprocessable_entity
+      timestamp: Time.current.iso8601 
+    }, status: :ok
   end
 
   def stream
