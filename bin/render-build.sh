41aab51e3cc63108cc8b3d301b7511b9ee55829d
bdt17 <brett.thomas29.97@gmail.com>
2026-01-29 13:02:39 -0700

    Add Rails 8.1 render-build.sh - fix assets:precompile

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,5 +3,4 @@ set -o errexit
 
 bundle install
 bundle exec rails assets:precompile RAILS_ENV=production
-bundle exec rails assets:clean RAILS_ENV=production
 bundle exec rails db:migrate RAILS_ENV=production
