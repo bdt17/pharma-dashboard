c1e78aaf7b8be84820b5abb81ff983d835327b6a
bdt17 <brett.thomas29.97@gmail.com>
2026-01-30 20:37:47 -0700

    Phase 2 🚛 VehiclesController#index LIVE

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,9 +1,5 @@
 class VehiclesController < ApplicationController
-  def show
-    @vehicle_id = params[:id]
-    @status = "Active"
-    @eta = "8min"
-    @location = "Scottsdale"
-    render layout: false
+  def index
+    @vehicles = Vehicle.all
   end
 end
