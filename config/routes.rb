b9ab03b6b9f91b0b9684eb131b70d03e469c6794
bdt17 <brett.thomas29.97@gmail.com>
2026-02-04 18:59:12 -0700

    Add pharma logistics endpoints + clean monitor

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,8 @@ Rails.application.routes.draw do
   get '/gps_stream', to: ->(env) { [200, {"Content-Type" => "text/plain"}, ['GPS Stream']] }
   get '/test_pdf', to: ->(env) { [200, {"Content-Type" => "text/plain"}, ['PDF OK']] }
 end
+
+# NEW pharma logistics endpoints  
+get '/shipments', to: ->(env) { [200, {"Content-Type" => "application/json"}, ['{"shipments":42,"status":"active"}']] }
+get '/trucks', to: ->(env) { [200, {"Content-Type" => "application/json"}, ['{"trucks":25,"online":23}']] }
+get '/routes', to: ->(env) { [200, {"Content-Type" => "application/json"}, ['{"routes":156,"optimized":142}']] }
