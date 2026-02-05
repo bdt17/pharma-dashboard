e4eb2e0ab78980299853b37ea275e47763761667
bdt17 <brett.thomas29.97@gmail.com>
2026-02-04 21:34:55 -0700

    add health api + gps endpoints + fix test_ui

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -1,9 +1,22 @@
 class GpsController < ApplicationController
-  def create   # POST /gps_post
-    render json: { status: "GPS received", time: Time.now.utc }, status: 201
+  def vehicles
+    @vehicles = Vehicle.all
+    render json: @vehicles.map { |v| { 
+      id: v.id, 
+      license_plate: v.license_plate,
+      lat: v.lat, 
+      lng: v.lng,
+      status: v.status 
+    } }
   end
-  
-  def stream   # GET /gps_stream
-    render plain: "GPS Stream Active", status: 200
+
+  def batches
+    @batches = Batch.all
+    render json: @batches.map { |b| { 
+      id: b.id, 
+      name: b.name,
+      vehicle: b.vehicle.license_plate,
+      temp_status: b.temp_status
+    } }
   end
 end
