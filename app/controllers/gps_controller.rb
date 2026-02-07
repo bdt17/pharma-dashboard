7c2f29721dfb8e2b844822967b5ceba9c54f7559
bdt17 <brett.thomas29.97@gmail.com>
2026-02-07 16:14:51 -0700

    Missing Phase 2 controllers

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -1,10 +1,8 @@
 class GpsController < ApplicationController
   skip_before_action :verify_authenticity_token
-
   def update
-    head :no_content
+    head :no_content 
   end
-
   def stream
     render plain: "GPS LIVE"
   end
