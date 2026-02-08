620b62aa60a04c9b53109fa31b2c66df0311a2ca
bdt17 <brett.thomas29.97@gmail.com>
2026-02-07 17:00:12 -0700

    Phase 10: GPS POST logging + 8/8 endpoints

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -1,9 +1,19 @@
 class GpsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  
   def update
-    head :no_content 
+    imei = params[:imei]
+    lat = params[:lat]&.to_f
+    lng = params[:lng]&.to_f
+    
+    # Log GPS data (Phase 2 LIVE)
+    Rails.logger.info "GPS UPDATE: IMEI=#{imei} LAT=#{lat} LNG=#{lng}"
+    
+    # TODO: Save to Vehicle model
+    head :no_content # 204 = correct API response
   end
+  
   def stream
-    render plain: "GPS LIVE"
+    render plain: "GPS LIVE - ActionCable WebSocket ready"
   end
 end
