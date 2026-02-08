195c50b46999c8cc92d91115186ae4716c722de6
bdt17 <brett.thomas29.97@gmail.com>
2026-02-07 18:44:00 -0700

    feat: Live Phoenix fleet + revenue dashboard (Phase 8 100%) 🚀💰

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_08_010300) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_08_014141) do
   create_table "alerts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "message"
