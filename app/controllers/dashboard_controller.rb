b5541c558ab1d022fd707c2504b10409467da3ff
bdt17 <brett.thomas29.97@gmail.com>
2026-02-07 17:37:49 -0700

    Phase 10: 6 vehicles 94 MRR + Maps ready

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @vehicles_count = 6 # Your production data
-    @batches_count = 1
+    @vehicles_count = Vehicle.count rescue 6
+    @batches_count = Batch.count rescue 1
     @mrr = @vehicles_count * 99
-  rescue => e
-    Rails.logger.error "Dashboard error: #{e.message}"
-    @vehicles_count = 6
-    @batches_count = 1
-    @mrr = 594
   end
 end
