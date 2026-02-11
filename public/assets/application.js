99fc1c7bab62a13281331ee876aa81f53ace6f6b
bdt17 <brett.thomas29.97@gmail.com>
2026-02-10 18:59:51 -0700

    Remove node_modules + secrets

--- public/assets/application.js
+++
@@ -1,5 +0,0 @@
-import { Application } from "@hotwired/stimulus"
-import { registerControllersFrom } from "@hotwired/stimulus-loading"
-
-const application = Application.start()
-registerControllersFrom("controllers", application)
