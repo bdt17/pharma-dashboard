7b927b84013cc077da23a79cebd223e055fc60df
bdt17 <brett.thomas29.97@gmail.com>
2026-02-11 23:43:11 -0700

    🔥 MINIMAL ROUTES + NO INITIALIZERS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,3 +5,11 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.log_level = :info
 end
+
+# Disable eager loading causing crashes
+config.eager_load = false
+config.cache_classes = true
+
+# Disable asset pipeline
+config.assets.compile = false
+config.assets.debug = false
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get "/" => lambda { [200, {"Content-Type" => "text/html"}, ["PHARMA DASHBOARD v8.1 LIVE"]] }
-  get "/health" => lambda { [200, {"Content-Type" => "application/json"}, ['{"status":"ok"}']] }
-  get "/api/health" => lambda { [200, {"Content-Type" => "application/json"}, ['{"status":"ok"}']] }
-  get "/vehicles" => lambda { [200, {"Content-Type" => "application/json"}, ['[]']] }
-  get "/batches" => lambda { [200, {"Content-Type" => "application/json"}, ['[]']] }
-  post "/gps/update" => lambda { [200, {"Content-Type" => "application/json"}, ['{"received":true}']] }
-  get "/gps/stream" => lambda { [200, {"Content-Type" => "text/plain"}, [""]] }
+  get '/' => proc { [200, {'Content-Type' => 'text/html'}, ['PHARMA DASHBOARD v8.1']] }
+  get '/health' => proc { [200, {'Content-Type' => 'application/json'}, ['{"status":"ok"}']] }
+  get '/api/health' => proc { [200, {'Content-Type' => 'application/json'}, ['{"status":"ok"}']] }
+  get '/vehicles' => proc { [200, {'Content-Type' => 'application/json'}, ['[]']] }
+  get '/batches' => proc { [200, {'Content-Type' => 'application/json'}, ['[]']] }
+  post '/gps/update' => proc { [200, {'Content-Type' => 'application/json'}, ['{"received":true}']] }
+  get '/gps/stream' => proc { [200, {'Content-Type' => 'text/plain'}, ['']] }
 end
