d601318b3eca8533d675707f5cb3e4faaeba6805
bdt17 <brett.thomas29.97@gmail.com>
2026-02-11 18:15:11 -0700

    FIX: Rails 8 minimal production.rb + correct build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-rm -rf public/assets tmp/cache/assets
-bundle lock --add-platform x86_64-linux
 bundle install
-yarn install --frozen-lockfile
-bundle exec rails javascript:build
-bundle exec rails css:build
+yarn install --production || true
 bundle exec rails db:prepare
