997ac7d72c341382d949883371be9953d67b2f34
bdt17 <brett.thomas29.97@gmail.com>
2026-02-14 14:04:19 -0700

    Add pharma login page + admin user - Phase 8 LIVE

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/vehicles', to: 'application#vehicles'
   get '/batches', to: 'application#batches'
   post '/gps_update', to: 'application#gps_update'
+  get '/billing', to: 'application#billing'
   
   devise_for :users
   root to: 'application#index'
