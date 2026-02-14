79b40312ce44cd7d3d4d5a0fe9f7882a25e98aa1
bdt17 <brett.thomas29.97@gmail.com>
2026-02-14 16:52:40 -0700

    Fix dashboard route + disable broken Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get '/', to: 'application#index'
+  get '/',          to: 'application#index'
   get '/dashboard', to: 'application#dashboard'
-  get '/health', to: 'application#health'
-  get '/vehicles', to: 'application#vehicles'
-  get '/batches', to: 'application#batches'
+  get '/health',    to: 'application#health'
+  get '/vehicles',  to: 'application#vehicles'
+  get '/batches',   to: 'application#batches'
+  get '/compliance',to: 'application#compliance'
+  get '/billing',   to: 'application#billing'
   post '/gps_update', to: 'application#gps_update'
-  get '/billing', to: 'application#billing'
-  get '/compliance', to: 'application#compliance'
   
-  devise_for :users
+  # Devise - COMMENT OUT UNTIL PROPERLY INSTALLED
+  # devise_for :users
+  
   root to: 'application#index'
 end
