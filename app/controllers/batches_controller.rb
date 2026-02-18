049635a66a371cbce6b4c6184017e5e403a3d57d
bdt17 <brett.thomas29.97@gmail.com>
2026-02-17 21:02:09 -0700

    FINAL FIX: Remove render html: completely

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,5 +1,4 @@
 class BatchesController < ApplicationController
   def index
- # LAYOUT WORKS NOW - renders app/views/batches/index.html.erb
   end
 end
--- app/controllers/compliance_controller.rb
+++ app/controllers/compliance_controller.rb
@@ -1,5 +1,4 @@
 class ComplianceController < ApplicationController
   def index
- # LAYOUT WORKS NOW - renders app/views/compliance/index.html.erby
   end
 end
