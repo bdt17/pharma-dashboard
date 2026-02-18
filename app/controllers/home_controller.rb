ae561ebfe221ef630c2085fea46aee62503fad71
bdt17 <brett.thomas29.97@gmail.com>
2026-02-17 19:36:22 -0700

    Split controllers + add html_safe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
+# app/controllers/home_controller.rb
 class HomeController < ApplicationController
   def index
-  end
-
-  def health
-  end
-
-  def dashboard
+    render html: '<h1>Pharma Transport Dashboard</h1><p>$500K ARR Ready</p>'.html_safe
   end
 end
