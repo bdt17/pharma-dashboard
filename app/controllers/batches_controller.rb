651dae4fdbcd13e904d3081d7519422be6e905ef
bdt17 <brett.thomas29.97@gmail.com>
2026-02-18 16:36:45 -0700

    Backup old + fix 500s with 7/7 endpoints

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,4 +1,22 @@
 class BatchesController < ApplicationController
   def index
+    @batches = Batch.all
+  end
+
+  def show
+    @batch = Batch.find(params[:id])
+  end
+
+  def chain_of_custody
+    @batch = Batch.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "custody_#{@batch.id}",
+               template: "batches/chain_of_custody",
+               layout: "pdf.html",
+               page_size: "A4"
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
-# config/routes.rb
 Rails.application.routes.draw do
-  resources :batches    # Line 13 should be here
-  resources :vehicles
-  resources :compliance
-  # ... other resources
-  root 'home#index'     # or whatever your home page is
+  root 'home#index'
+  
   get 'health', to: 'health#index'
+  get 'billing', to: 'billing#index'
+
+  # Dashboard resources  
+  resources :vehicles, only: [:index]
+  resources :batches, only: [:index, :show] do
+    get :chain_of_custody, on: :member
+  end
+  resources :compliance, only: [:index]
+
+  # Authentication
   devise_for :users
 end
