fcb114b4e69fc6835c6ae35eacee8501eb36383a
bdt17 <brett.thomas29.97@gmail.com>
2026-02-19 18:50:35 -0700

    Phase 8 complete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.stripe.secret_key = ENV['STRIPE_SECRET_KEY'] || 'sk_test_xxx' # dummy for dev
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
