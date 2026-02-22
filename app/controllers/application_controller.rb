14790250c2083acbe78d6b908e31aa92a41fb96a
bdt17 <brett.thomas29.97@gmail.com>
2026-02-22 15:09:51 -0700

    Fix SECRET_KEY_BASE + add pharma landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
-  # DISABLE GLOBAL AUTH - Enterprise endpoints first
   skip_before_action :authenticate_user!
-  
-  # Devise Turbo compatibility
   protect_from_forgery prepend: true
   
-  # Re-enable auth only for user routes later
+  def root
+  end
 end
