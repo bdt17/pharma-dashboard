c70231ac1ce4e372906cfdc32644b92c599a0aa0
bdt17 <brett.thomas29.97@gmail.com>
2026-02-22 21:55:47 -0700

    PHASE 8 ✅ PDF Chain-of-Custody LIVE

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "reports/pdf"
   get "health/index"
   get "home/index"
   # Root
@@ -23,3 +24,6 @@ Rails.application.routes.draw do
   # Enterprise
   get '/billing', to: 'billing#index'
 end
+  member do
+    get :pdf, action: :pdf
+  end
