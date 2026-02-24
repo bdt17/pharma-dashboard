e935049ab6c80400633307b0b71431cd9af45bdf
bdt17 <brett.thomas29.97@gmail.com>
2026-02-23 18:58:26 -0700

    fix(devise): configure existing User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,3 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+
+devise :database_authenticatable, :registerable,
+       :recoverable, :rememberable, :validatable
