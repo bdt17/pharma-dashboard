6bd4c02f00a5a336b317eac3dcd2853521b8a746
bdt17 <brett.thomas29.97@gmail.com>
2026-02-24 22:01:04 -0700

    Add admin user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-User.destroy_all
 User.create!(
   email: 'admin@pharmagps.com',
-  password: 'P@ssw0rdPh@rm@2026!v14',
-  password_confirmation: 'P@ssw0rdPh@rm@2026!v14'
+  password: 'password123',
+  password_confirmation: 'password123'
 )
-puts "✅ Admin created: P@ssw0rdPh@rm@2026!v14"
+puts "✅ Admin created: admin@pharmagps.com / password123"
