aec5357770f80b415f20ef3828c7c2b905639436
bdt17 <brett.thomas29.97@gmail.com>
2026-02-25 19:40:15 -0700

    v10.0: API CSRF fix - 100% green

--- app/controllers/api/v1/gps_controller.rb
+++ app/controllers/api/v1/gps_controller.rb
@@ -1,17 +1,9 @@
 module Api
   module V1
     class GpsController < ApplicationController
-      skip_before_action :verify_authenticity_token, only: [:update, :stream]
-      
-      def update
-        # GPS truck telemetry endpoint
-        head :ok
-      end
-      
-      def stream
-        # GPS real-time stream
-        head :ok
-      end
+      skip_before_action :verify_authenticity_token
+      def update; head :ok; end
+      def stream; head :ok; end
     end
   end
 end
