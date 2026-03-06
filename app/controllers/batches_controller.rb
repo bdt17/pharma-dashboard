83f1bc3d52f5b81e03c5943244e426e1f87cf32c
bdt17 <brett.thomas29.97@gmail.com>
2026-03-05 20:35:10 -0700

    fix: batches#index PDF support - no more 406

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,16 +1,37 @@
 class BatchesController < ApplicationController
   before_action :set_batch, only: [:show, :custody_report, :coc_pdf, :edit, :update, :destroy]
 
-  def index
-    @batches = Batch.all
-    respond_to do |format|
-      format.html
-      format.pdf do
-        render plain: "Batch list PDF - #{@batches.count} batches", 
-               content_type: "application/pdf"
-      end
+def index
+  @batches = Batch.all
+  respond_to do |format|
+    format.html
+    format.pdf do
+      # Raw PDF - matches your chain-of-custody style
+      pdf_content = <<~PDF
+%PDF-1.4
+1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj
+2 0 obj<</Type/Pages/Kids[3 0 R]/Count 1>>endobj
+3 0 obj<</Type/Page/MediaBox[0 0 612 792]/Parent 2 0 R/Contents 4 0 R/Resources<</Font<</F1 5 0 R>>endobj
+4 0 obj<</Length 500>>stream
+BT /F1 20 Tf
+80 750 Td (PHARMA TRANSPORT - BATCHES LIST) Tj
+80 720 Td (Generated: #{Time.now.utc.strftime('%Y-%m-%d %H:%M UTC')}) Tj
+80 690 Td (Total Batches: #{@batches.count}) Tj
+80 660 Td (Active: #{@batches.where(status: 'active').count}) Tj
+80 630 Td (In Transit: #{@batches.where(status: 'in_transit').count}) Tj
+80 600 Td (Completed: #{@batches.where(status: 'completed').count}) Tj
+ET endstream endobj
+5 0 obj<</Type/Font/Subtype/Type1/BaseFont/Helvetica>>endobj
+trailer<</Size 6/Root 1 0 R>>%%EOF
+      PDF
+
+      send_data pdf_content,
+        filename: "Pharma-Batches-#{Date.today}.pdf",
+        type: 'application/pdf',
+        disposition: 'attachment'
     end
   end
+end
 
   def show
     respond_to do |format|
@@ -46,6 +67,26 @@ class BatchesController < ApplicationController
     @batch.destroy
     redirect_to batches_path, notice: 'Batch deleted.'
   end
+
+def batches_pdf
+  @batches = Batch.all
+  respond_to do |format|
+    format.pdf do
+      pdf = render_to_string(template: 'batches/index', formats: [:html])
+      send_data pdf, 
+        filename: "Pharma-Batches-#{Date.today}.pdf",
+        type: 'application/pdf',
+        disposition: 'attachment'
+    end
+  end
+end
+
+def custody_report
+  respond_to do |format|
+    format.html { render 'chain_of_custody' }
+    format.pdf  { render :custody_report_pdf } # Your raw PDF
+  end
+end
 
   def custody_report
     respond_to do |format|
