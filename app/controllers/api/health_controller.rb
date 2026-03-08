533d2858877fef454b44485cf7ec19a0fe8a9015
bdt17 <brett.thomas29.97@gmail.com>
2026-03-07 21:59:56 -0700

    Add API::HealthController

--- app/controllers/api/health_controller.rb
+++ app/controllers/api/health_controller.rb
@@ -1,11 +1,10 @@
 class Api::HealthController < ApplicationController
   def index
-    render json: {
-      status: 'OK',
-      timestamp: Time.now.utc.iso8601,
-      batches: Batch.count,
-      vehicles: Vehicle.where(status: 'online').count,
-      version: 'v8.1'
+    render json: { 
+      status: 'ok', 
+      uptime: Rails.env.production? ? 'production' : 'development',
+      timestamp: Time.current.iso8601,
+      version: Rails.version 
     }
   end
 end
