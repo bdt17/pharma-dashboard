fe80722efc47a9fc0dc1d48b4f0913f02e49c4bb
bdt17 <brett.thomas29.97@gmail.com>
2026-03-08 19:30:44 -0700

    ✅ Phase 10 LIVE: GPS Queclink GV55 Vehicle #1 + Clean routes + GpsEvent model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_05_225747) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_09_022512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -95,6 +95,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_05_225747) do
     t.string "name"
     t.string "phone"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "gps_events", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "imei"
+    t.float "latitude"
+    t.float "longitude"
+    t.float "temperature"
+    t.datetime "updated_at", null: false
+    t.integer "vehicle_id"
   end
 
   create_table "location_points", force: :cascade do |t|
