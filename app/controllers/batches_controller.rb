b6fc1d01c6079040345062dd47242847468bf9e7
bdt17 <brett.thomas29.97@gmail.com>
2026-03-10 12:27:36 -0700

    Fix batches controller - proper PDF respond_to

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,5 +1,17 @@
 class BatchesController < ApplicationController
+  respond_to :html, :pdf
+
   def index
-    render plain: "Batches Dashboard - 5 Active (LOT-PHARMA-20260217)"
+    @batches = Batch.all  # Your DEA chain-of-custody batches
+    respond_to do |format|
+      format.html { render 'batches/index' }
+      format.pdf do
+        render pdf: "batches_#{Date.today}",
+               template: "batches/index",
+               layout: "pdf",
+               disposition: "attachment",
+               encoding: "UTF-8"
+      end
+    end
   end
 end
