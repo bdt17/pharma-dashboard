c5f1ce3d012d5d7f0637b77fc0b56050182fd145
bdt17 <brett.thomas29.97@gmail.com>
2026-03-11 19:20:25 -0700

    ✅ Clean routes - Stripe + GPS Queclink ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,17 @@
 Rails.application.routes.draw do
   root "home#index"
-  
+
   get "health", to: "health#index"
   get "dashboard", to: "dashboard#index"
+  
   resources :batches do
     member do
       get :chain_of_custody, defaults: {format: 'pdf'}
     end
   end
+  
   get "gps/update", to: "gps#update"
   get "subscribe", to: "subscriptions#new"
   get "stripe/new", to: "stripe#new"
+  post "gps/receive", to: "gps#receive"  # ← GPS endpoint
 end
-
-get "stripe/new", to: "stripe#new"
-get "stripe/new", to: "stripe#new"
-
-get "stripe/new", to: "stripe#new"
-get "gps/update", to: "gps#update"
-get "stripe/new", to: "stripe#new"
-get "stripe/new", to: "stripe#new"
-get "stripe/new", to: "stripe#new"
-get "gps/update", to: "gps#update"
-get '/stripe/new', to: 'stripe#new'
-post '/gps/receive', to: 'gps#receive'
