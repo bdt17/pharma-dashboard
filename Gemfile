9edd0e66bea8c6b4789290bc5d189a6eb0d04883
bdt17 <brett.thomas29.97@gmail.com>
2026-03-12 19:24:50 -0700

    FINAL: Gemfile.lock + 8/8 controllers

--- Gemfile
+++ Gemfile
@@ -7,7 +7,3 @@ gem "puma", "~> 5.0"
 gem "jbuilder"
 gem "bootsnap", require: false
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
-
-group :development, :test do
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
-end
--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -1,10 +1,8 @@
 class GpsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
   def update
     render plain: "GPS OK", status: 200
   end
-  
   def stream
     render plain: "GPS STREAM OK", status: 200
   end
