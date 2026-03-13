0fb4912be822f573a487129d0fe100d7dce99653
bdt17 <brett.thomas29.97@gmail.com>
2026-03-12 21:42:25 -0700

    Thomas IT footer only + Remove devise_for crash

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
   root "dashboard#index"
-  
-  # Public endpoints
   get "health", to: "health#index"
   get "dashboard", to: "dashboard#index"
   get "batches", to: "batches#index"
-  get "batches.pdf", to: "batches#index"
   get "batches/:id/chain-of-custody.pdf", to: "batches#show"
   post "gps/update", to: "gps#update"
   get "gps", to: "gps#index"
   get "subscribe", to: "subscribe#index"
   get "stripe", to: "stripe#index"
-  
-  # Devise routes (login)
-  devise_for :users
 end
