e14eadfb2a494668a1ba2ac613f829f49a3e699d
bdt17 <brett.thomas29.97@gmail.com>
2026-03-12 19:43:56 -0700

    EMERGENCY: Kill propshaft from application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,12 @@
 require_relative "boot"
+
 require "rails/all"
-require "propshaft"
 
 Bundler.require(*Rails.groups)
 
 module PharmaDashboard
   class Application < Rails::Application
-    config.load_defaults 8.1
-    config.i18n.default_locale = :en
-    config.i18n.available_locales = [:en]
-    config.i18n.fallbacks = [:en]
+    config.load_defaults 7.0
     config.api_only = false
   end
 end
-
-
